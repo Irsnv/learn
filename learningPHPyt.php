959f4f76985323a025d7ea63edecbe93338c8959
Muhammad Irsan <155605371+Irsnv@users.noreply.github.com>
2025-01-24 20:21:41 +0800

    Add files via upload

---
+++ learningPHPyt.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Irsan Larning YT</title>
+<!-- Links to our stylesheets -->
+<!-- <Link rel="stylesheets" href="css/reset.css"> -->
+<Link rel="stylesheets" href="css/main.css">
+</head>
+
+<body>
+
+    <main>
+    $amp   
+        <form action="includes/learningPHPyt.php" method="post">
+            <label for="firstname">First Name?</label>
+            
+
+            <input id="first name" type="text" name="firstname" placeholder="First name...">
+
+            <label for="lastname">Last Name?</label>
+            <input id="Last name" type="text" name="lastname" placeholder="Last name...">
+            
+            <br><br>
+
+            <label for="favouritepet">Favourite Pet?</label>
+            <select id="favouritepet" name="favouritepet">
+                <option value="none">None</option>
+                <option value="dog">Dog</option>
+                <option value="cat">Cat</option>
+                <option value="piranha">Piranha</option>
+            </select>
+
+            <botton types="submit">Submit</button>
+
+        </form>
+    </main>
+</body>
+
+
+</html>
